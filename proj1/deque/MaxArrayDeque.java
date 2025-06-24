cb6e0ead6afdc280dc01dde0e42b94b27f256efb
tsk <3201557425@qq.com>
2025-06-24 19:27:49 +0800

    fix the bugs in MaxArrayDeque, now it can return max correctly. Don't submit! time to think of how we can make arraylist: add/removeFirst use constant time.

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -1,24 +1,24 @@
 package deque;
+import edu.princeton.cs.algs4.StdRandom;
+
 import java.util.Comparator;
 
 public class MaxArrayDeque<T> extends ArrayDeque<T> {
-    private ArrayList<T> maxdeque;
     private Comparator<T> c;
 
     public MaxArrayDeque(Comparator<T> com) {
-        maxdeque = new ArrayList<>();
         c = com;
     }
 
     /**  Returns the maximum element by this.c */
     public T max() {
-        if (maxdeque.getSize() == 0) {
+        if (size() == 0) {
             return null;
         }
-        T maxn = maxdeque.get(0);
-        for (int i = 1; i < maxdeque.getSize(); i++) {
-            if (c.compare(maxn, maxdeque.get(i)) < 0) {
-                maxn = maxdeque.get(i);
+        T maxn = get(0);
+        for (int i = 1; i < size(); i++) {
+            if (c.compare(maxn, get(i)) < 0) {
+                maxn = get(i);
             }
         }
         return maxn;
@@ -26,17 +26,43 @@ public class MaxArrayDeque<T> extends ArrayDeque<T> {
 
     /** Returns the maximum element by the given comparator com. */
     public T max(Comparator<T> com) {
-        if (maxdeque.getSize() == 0) {
+        if (size() == 0) {
             return null;
         }
-        T maxn = maxdeque.get(0);
-        for (int i = 1; i < maxdeque.getSize(); i++) {
-            if (com.compare(maxn, maxdeque.get(i)) < 0) {
-                maxn = maxdeque.get(i);
+        T maxn = get(0);
+        for (int i = 1; i < size(); i++) {
+            if (com.compare(maxn, get(i)) < 0) {
+                maxn = get(i);
             }
         }
         return maxn;
     }
+
+
+    public static class IntCompare implements Comparator<Integer> {
+        @Override
+        public int compare(Integer a, Integer b) {
+            return a-b;
+        }
+    }
+    public static class IntComparesmall implements Comparator<Integer> {
+        @Override
+        public int compare(Integer a, Integer b) {
+            return b-a;
+        }
+    }
+
+//    public static void main(String[] args){
+//        IntCompare c1 = new IntCompare();
+//        IntComparesmall c2 = new IntComparesmall();
+//        MaxArrayDeque<Integer> d = new MaxArrayDeque<>(c1);
+//        for (int i=1;i<=100;i++) {
+//            d.addLast(StdRandom.uniform(0,500));
+//        }
+//        d.printDeque();
+//        System.out.println(d.max());
+//        System.out.println(d.max(c2));
+//    }
 
 
 }
