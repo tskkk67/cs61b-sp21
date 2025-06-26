ea84c775e66a31d831f6e5f9c6288d5904ab7c87
tsk <3201557425@qq.com>
2025-06-26 20:53:57 +0800

    lab4 debugging first try, Integer -> int

--- lab4/flik/HorribleSteve.java
+++ lab4/flik/HorribleSteve.java
@@ -1,9 +1,10 @@
 package flik;
 
+
 public class HorribleSteve {
-    public static void main(String [] args) throws Exception {
-        int i = 0;
-        for (int j = 0; i < 500; ++i, ++j) {
+    public static void main(String[] args) throws Exception {
+        int i = 129;
+        for (int j = 129; i < 500; ++i, ++j) {
             if (!Flik.isSameNumber(i, j)) {
                 throw new Exception(
                         String.format("i:%d not same as j:%d ??", i, j));
